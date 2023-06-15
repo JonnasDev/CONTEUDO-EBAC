5c1ad2cc55594d2d401ab04e0799ce13d87a352a
Jonnas <queopsbr00013@gmail.com>
2023-06-15 14:55:26 -0300

    final da aula 2

---
+++ src/pages/video.js
@@ -0,0 +1,36 @@
+import React, { useRef, useState } from 'react'
+import './video.css'
+
+function ComponenteVideo() {
+
+  const videoRef = useRef(null)
+  const [play, setplay] = useState(false)
+
+  function handleStart() {
+
+    if (play) {
+      videoRef.current.pause()
+      setplay(false)
+    }
+    else {
+      videoRef.current.play()
+      setplay(true)
+    }
+
+  }
+
+  return (
+    <div className='video'>
+
+      <video className='video-player'
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z" >
+      </video>
+
+    </div>
+  )
+}
+
+export default ComponenteVideo
