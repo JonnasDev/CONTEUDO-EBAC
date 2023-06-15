12d430e7cfd3bee54dbfc2b96ce35192f00fb13f
Jonnas <queopsbr00013@gmail.com>
2023-06-15 15:05:32 -0300

    final aula 2

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import ComponenteVideo from './pages/video';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app-video'>
+        <ComponenteVideo/>
+        <ComponenteVideo/>
+        <ComponenteVideo/>
+      </div>
     </div>
   );
 }
